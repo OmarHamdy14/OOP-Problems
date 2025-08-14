8c8a2228a6559631e7850ccc10696cac420ec749
OmarHamdy <omarwasly003@gmail.com>
2025-08-14 17:07:21 +0300

    P11

--- OOP.Problems/OOP_P11.cpp
+++ OOP.Problems/OOP_P11.cpp
@@ -9,3 +9,97 @@ using namespace std;
 Model an organization where different roles have access to different tools. Tools perform operations, and roles use them differently. 
 Ensure that each role only uses the tools allowed for them.
 */
+
+// =======================================> "Has-a relationship" <=======================================
+
+
+class Tool {
+public:
+	string Name;
+	Tool(string N) : Name(N) {}
+	virtual void work(string& userRole) = 0;
+};
+class Tool_A : public Tool {
+public:
+	Tool_A(string n) : Tool(n){}
+	void work(string& userRole) override{
+		cout << userRole << " use " << Name << endl;
+	}
+};
+class Tool_B : public Tool {
+public:
+	Tool_B(string n) : Tool(n) {}
+	void work(string& userRole) override {
+		cout << userRole << " use " << Name << endl;
+	}
+};
+class Tool_C : public Tool {
+public:
+	Tool_C(string n) : Tool(n) {}
+	void work(string& userRole) override {
+		cout << userRole << " use " << Name << endl;
+	}
+};
+
+
+
+class Role {
+public:
+	vector<shared_ptr<Tool>> AllowedTools;
+	string Name;
+	Role(string n) : Name(n) {}
+	virtual void addTool(shared_ptr<Tool> tool) = 0;
+	virtual void DisplayWorkAllowedTools() = 0;
+};
+class Manager : public Role {
+public:
+	Manager(string n) : Role(n) {}
+	void addTool(shared_ptr<Tool> tool) override {
+		AllowedTools.push_back(tool);
+	}
+	void DisplayWorkAllowedTools() override {
+		for (auto& tool : AllowedTools) {
+			cout << tool->Name << endl;
+			tool->work(Name);
+		}
+	}
+};
+class SemiManager : public Role {
+public:
+	SemiManager(string n) : Role(n) {}
+	void addTool(shared_ptr<Tool> tool) override {
+		AllowedTools.push_back(tool);
+	}
+	void DisplayWorkAllowedTools() override {
+		for (auto& tool : AllowedTools) {
+			cout << tool->Name << endl;
+			tool->work(Name);
+		}
+	}
+};
+class Assistant : public Role {
+public:
+	Assistant(string n) : Role(n) {}
+	void addTool(shared_ptr<Tool> tool) override {
+		AllowedTools.push_back(tool);
+	}
+	void DisplayWorkAllowedTools() override {
+		for (auto& tool : AllowedTools) {
+			cout << tool->Name << endl;
+			tool->work(Name);
+		}
+	}
+};
+
+int main() {
+	shared_ptr<Tool> toolA = make_shared<Tool_A>();
+	shared_ptr<Tool> toolB = make_shared<Tool_B>();
+	shared_ptr<Tool> toolC = make_shared<Tool_C>();
+
+	Manager OmarManager("OmarManager");
+	SemiManager OmarSemiManager("OmarSemiManager");
+	Assistant OmarAssistant("OmarAssistant");
+
+	OmarManager.addTool(toolA);
+	OmarManager.DisplayWorkAllowedTools();
+}
---
+++ OOP.Problems/OOP_P12.cpp
@@ -0,0 +1,11 @@
+#include "OOP_P12.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+using namespace std;
+/*
+* Problem Statement
+Simulate a tournament where participants have different abilities. Each round, they perform actions, 
+and their performance affects their standing. Include a system to manage rounds, scores, and eliminations.
+*/ 

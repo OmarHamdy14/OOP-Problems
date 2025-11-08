f7206848b3d056c386b45d20b47280dbe3476ef5
OmarHamdy <omarwasly003@gmail.com>
2025-11-08 18:38:06 +0200

    P46

---
+++ OOP.Problems/P46.cpp
@@ -0,0 +1,39 @@
+#include "P46.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/*
+* Problem Statement
+Emergency Response Simulator
+*/
+
+class Unit {
+protected:
+	string describtion;
+public:
+
+};
+class Police : public Unit {
+
+};
+class Fire : public Unit {
+
+};
+class Ambulance : public Unit {
+
+};
+
+class Incident {
+
+};
+
+class Dispatcher {
+
+};

9fd6712754aababa9dec727301948e44d4c7fb2c
OmarHamdy <omarwasly003@gmail.com>
2025-11-09 16:18:09 +0200

    P46

--- OOP.Problems/P46.cpp
+++ OOP.Problems/P46.cpp
@@ -18,22 +18,49 @@ class Unit {
 protected:
 	string describtion;
 public:
+	Unit(const string& n) : describtion(n) {}
+	const string& Getdescribtion() const { return describtion; }
+	void Setdescribtion(const string& t) { describtion = t; }
 
+	virtual void Work() const = 0;
 };
 class Police : public Unit {
-
+public:
+	Police(const string& n) : Unit(n) {}
+	void Work() const {
+		cout << "Police ...... work\n";
+	}
 };
 class Fire : public Unit {
-
+public:
+	Fire(const string& n) : Unit(n) {}
+	void Work() const {
+		cout << "Fire ...... work\n";
+	}
 };
 class Ambulance : public Unit {
-
+public:
+	Ambulance(const string& n) : Unit(n) {}
+	void Work() const {
+		cout << "Ambulance ...... work\n";
+	}
 };
 
 class Incident {
-
+	string describtion;
+public:
+	Incident(const string& n) : describtion(n) {}
+	const string& Getdescribtion() const { return describtion; }
+	void Setdescribtion(const string& t) { describtion = t; }
 };
 
 class Dispatcher {
-
+	string Name;
+public:
+	Dispatcher(const string& n) : Name(n) {}
+	const string& GetName() const { return Name; }
+	void SetName(const string& t) { Name = t; }
+	void SendPolice(Incident& incd, shared_ptr<Police>& plc) {
+		cout << "Sending Police ..........\n";
+	}
 };

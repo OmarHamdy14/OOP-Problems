dd6ddcf279686af2069e69e31c70811625ce785d
OmarHamdy <omarwasly003@gmail.com>
2025-12-01 18:30:55 +0200

    P50

--- OOP.Problems/P50.cpp
+++ OOP.Problems/P50.cpp
@@ -10,5 +10,45 @@ using namespace std;
 
 /*
 * Problem Statement
-
+Space Exploration Simulation Spaceships have Engines, Crews, and Missions. 
+Different ship types specialize in exploration, transport, or research. 
+Mission control coordinates all ships and resources.
 */
+
+class Engine {
+    string type;
+public:
+    Engine(string t) : type(t) {}
+    string GetType() const { return type; }
+    void Start() { cout << "starting......... \n"; }
+};
+
+
+class CrewMember {
+    string name;
+    string role;
+public:
+    CrewMember(string n, string r) : name(n), role(r) {}
+    void Info() const { cout << role << ": " << name << "\n"; }
+};
+
+
+class Mission {
+public:
+    virtual void Execute() = 0;
+};
+
+class ExplorationMission : public Mission {
+public:
+    void Execute() override { cout << "exploration mission...\n"; }
+};
+
+class TransportMission : public Mission {
+public:
+    void Execute() override { cout << "transport mission...\n"; }
+};
+
+class ResearchMission : public Mission {
+public:
+    void Execute() override { cout << "research mission...\n"; }
+};

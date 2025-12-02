ac7b89c4f9fd6accbe51c6aff3d136c67be88947
OmarHamdy <omarwasly003@gmail.com>
2025-12-02 15:24:22 +0200

    P50

--- OOP.Problems/P50.cpp
+++ OOP.Problems/P50.cpp
@@ -18,7 +18,7 @@ Mission control coordinates all ships and resources.
 class Engine {
     string type;
 public:
-    Engine(string t) : type(t) {}
+    Engine(const string& t) : type(t) {}
     string GetType() const { return type; }
     void Start() { cout << "starting......... \n"; }
 };
@@ -28,7 +28,7 @@ class CrewMember {
     string name;
     string role;
 public:
-    CrewMember(string n, string r) : name(n), role(r) {}
+    CrewMember(const string& n, string r) : name(n), role(r) {}
     void Info() const { cout << role << ": " << name << "\n"; }
 };
 
@@ -51,4 +51,68 @@ public:
 class ResearchMission : public Mission {
 public:
     void Execute() override { cout << "research mission...\n"; }
+};
+
+class Spaceship {
+protected:
+    string name;
+    shared_ptr<Engine> engine;
+    vector<CrewMember> crew;
+    shared_ptr<Mission> mission;
+
+public:
+    Spaceship(const string& n, shared_ptr<Engine> e, shared_ptr<Mission> m)
+        : name(n), engine(e), mission(m) {}
+
+    void AddCrewMember(const CrewMember& cm) { crew.push_back(cm); }
+
+    virtual void Launch() {
+        cout << "Spaceship " << name << " launching........\n";
+        engine->Start();
+        mission->Execute();
+    }
+
+    void CrewInfo() const {
+        for (auto& c : crew) c.Info();
+    }
+};
+
+class ExplorerShip : public Spaceship {
+public:
+    ExplorerShip(const string& n, shared_ptr<Engine> e, shared_ptr<Mission> m)
+        : Spaceship(n, e, m) {}
+    void Launch() override {
+        cout << "Explorer ship launching......\n";
+    }
+};
+
+class TransportShip : public Spaceship {
+public:
+    TransportShip(const string& n, shared_ptr<Engine> e, shared_ptr<Mission> m)
+        : Spaceship(n, e, m) {}
+    void Launch() override {
+        cout << "Transport ship launching......\n";
+    }
+};
+
+class ResearchShip : public Spaceship {
+public:
+    ResearchShip(const string& n, shared_ptr<Engine> e, shared_ptr<Mission> m)
+        : Spaceship(n, e, m) {}
+    void Launch() override {
+        cout << "Research ship launching......\n";
+     }
+};
+
+
+class MissionControl {
+    vector<shared_ptr<Spaceship>> fleet;
+public:
+    void AddShip(shared_ptr<Spaceship> ship) { fleet.push_back(ship); }
+    void LaunchAll() {
+        for (auto& ship : fleet) {
+            ship->Launch();
+            cout << endl;
+        }
+    }
 };

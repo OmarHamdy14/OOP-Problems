2a933de665cfc74cef6fb020dae8d92776229e94
OmarHamdy14 <omarhamdy8003@gmail.com>
2026-01-13 19:34:11 +0200

    P61

--- OOP.Problems/P61.cpp
+++ OOP.Problems/P61.cpp
@@ -1,5 +1,4 @@
-﻿#include "P61.h"
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <algorithm>
 #include <vector>
@@ -68,5 +67,79 @@ class Test2 : public LabTest {
 public:
     string Result() const override {
         return "Test2: ...";
+    }
+};
+
+class Patient {
+    string name;
+    vector<MedicalRecord> records;
+    vector<unique_ptr<Prescription>> prescriptions;
+    vector<unique_ptr<LabTest>> tests;
+public:
+    Patient(const string& n) : name(n) {}
+
+    string GetName() const { return name; }
+
+    void AddRecord(const MedicalRecord& r) {
+        records.push_back(r);
+    }
+
+    void AddPrescription(unique_ptr<Prescription> p) {
+        prescriptions.push_back(move(p));
+    }
+
+    void AddLabTest(unique_ptr<LabTest> t) {
+        tests.push_back(move(t));
+    }
+
+    void ShowHistory() const {
+        cout << "\nPatient: " << name << endl;
+
+        for (auto& r : records)
+            cout << "Diagnosis: " << r.GetDiagnosis() << endl;
+
+        for (auto& p : prescriptions)
+            cout << p->Info() << endl;
+
+        for (auto& t : tests)
+            cout << t->Result() << endl;
+    }
+};
+
+
+class Treatment {
+public:
+    virtual ~Treatment() = default;
+    virtual void Apply(Patient& p) = 0;
+};
+
+class CardiologyTreatment : public Treatment {
+public:
+    void Apply(Patient& p) override {
+        p.AddPrescription(make_unique<DrugPrescription>("Heart Medication"));
+        p.AddLabTest(make_unique<BloodTest>());
+    }
+};
+
+class NeurologyTreatment : public Treatment {
+public:
+    void Apply(Patient& p) override {
+        p.AddPrescription(make_unique<TherapyPrescription>("Therapy"));
+        p.AddLabTest(make_unique<Test2>());
+    }
+};
+
+
+class Doctor {
+    string name;
+    unique_ptr<Treatment> treatment;
+public:
+    Doctor(const string& n, unique_ptr<Treatment> t)
+        : name(n), treatment(move(t)) {
+    }
+
+    void Treat(Patient& patient) {
+        cout << "\nDoctor " << name << " treating " << patient.GetName() << endl;
+        treatment->Apply(patient);
     }
 };

2a186ebd335c326bae59edf1ee47564a7b870e70
OmarHamdy14 <omarhamdy8003@gmail.com>
2026-01-25 18:47:41 +0200

    P64

--- OOP.Problems/P64.cpp
+++ OOP.Problems/P64.cpp
@@ -67,3 +67,73 @@ public:
         cout << name << " is delivering the order\n";
     }
 };
+
+enum class OrderStatus {
+    Placed,
+    Preparing,
+    OutForDelivery,
+    Delivered
+};
+
+class Order {
+    const Customer& customer;
+    const Restaurant& restaurant;
+    vector<MenuItem> items;
+    OrderStatus status = OrderStatus::Placed;
+
+public:
+    Order(const Customer& c, const Restaurant& r)
+        : customer(c), restaurant(r) {
+    }
+
+    void AddItem(const MenuItem& item) {
+        items.push_back(item);
+    }
+
+    double TotalPrice() const {
+        double total = 0;
+        for (auto& i : items)
+            total += i.price;
+        return total;
+    }
+
+    void UpdateStatus(OrderStatus newStatus) {
+        status = newStatus;
+    }
+
+    void AssignDelivery(const DeliveryAgent& agent) {
+        UpdateStatus(OrderStatus::OutForDelivery);
+        agent.Deliver();
+    }
+
+    void ShowSummary() const {
+        cout << "\nCustomer: " << customer.GetName() << endl;
+        cout << "Restaurant: " << restaurant.GetName() << endl;
+        cout << "Items:\n";
+        for (auto& i : items)
+            cout << "- " << i.name << " ($" << i.price << ")\n";
+
+        cout << "Total: $" << TotalPrice() << endl;
+    }
+};
+
+
+int main() {
+    Restaurant restaurant("Pizza");
+    restaurant.AddItem("basterma", 8.5);
+    restaurant.AddItem("chicken", 10);
+
+    Customer customer("Omar");
+    DeliveryAgent agent("mo");
+
+    Order order(customer, restaurant);
+
+    order.AddItem(restaurant.GetMenu()[0]);
+    order.AddItem(restaurant.GetMenu()[1]);
+
+    order.UpdateStatus(OrderStatus::Preparing);
+    order.AssignDelivery(agent);
+    order.UpdateStatus(OrderStatus::Delivered);
+
+    order.ShowSummary();
+}

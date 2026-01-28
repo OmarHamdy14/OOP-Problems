2954c4841696f0e72bb9068cf07cbd2662b908e5
OmarHamdy14 <omarhamdy8003@gmail.com>
2026-01-28 20:12:31 +0200

    P66

---
+++ OOP.Problems/P66.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <memory>
+#include <cstdlib>
+#include <ctime>
+#include <unordered_map>
+using namespace std;
+
+/*
+* Problem Statement
+Gym Membership System
+Members subscribe to plans.
+Plans differ in price and access.
+Trainers assign workouts.
+Attendance is tracked.
+*/

12670dbd9a40528318c4ba0bdf31fb755ad7421a
OmarHamdy14 <omarhamdy8003@gmail.com>
2026-01-29 19:47:52 +0200

    P66

--- OOP.Problems/P66.cpp
+++ OOP.Problems/P66.cpp
@@ -11,9 +11,104 @@ using namespace std;
 
 /*
 * Problem Statement
-Gym Membership System
-Members subscribe to plans.
-Plans differ in price and access.
-Trainers assign workouts.
-Attendance is tracked.
+Ride-Hailing App (Uber-like)
+Riders request rides.
+Drivers accept rides.
+Ride price depends on distance.
+Ride status updates in real time.
 */
+
+class PriceCalculator {
+public:
+    virtual ~PriceCalculator() = default;
+    virtual double Calculate(double distanceKm) const = 0;
+};
+
+class StandardPricing : public PriceCalculator {
+public:
+    double Calculate(double distanceKm) const override {
+        return 5.0 + distanceKm * 2.0;
+    }
+};
+
+
+enum class RideStatus {
+    Requested,
+    Accepted,
+    InProgress,
+    Completed
+};
+
+
+class Rider {
+    string name;
+
+public:
+    Rider(string n) : name(n) {}
+    string GetName() const { return name; }
+};
+
+class Driver {
+    string name;
+    bool available = true;
+
+public:
+    Driver(string n) : name(n) {}
+
+    bool IsAvailable() const { return available; }
+    void SetAvailable(bool a) { available = a; }
+
+    string GetName() const { return name; }
+};
+
+
+class Ride {
+    Rider& rider;
+    Driver* driver = nullptr;
+    double distance;
+    RideStatus status = RideStatus::Requested;
+    PriceCalculator& pricing;
+
+public:
+    Ride(Rider& r, double d, PriceCalculator& p)
+        : rider(r), distance(d), pricing(p) {
+    }
+
+    void AssignDriver(Driver& d) {
+        if (!d.IsAvailable()) {
+            cout << "Ddriver not available\n";
+            return;
+        }
+        driver = &d;
+        driver->SetAvailable(false);
+        status = RideStatus::Accepted;
+        cout << d.GetName() << " accepted the ride\n";
+    }
+
+    void StartRide() {
+        if (status == RideStatus::Accepted) {
+            status = RideStatus::InProgress;
+            cout << "Ride started\n";
+        }
+    }
+
+    void CompleteRide() {
+        if (status == RideStatus::InProgress) {
+            status = RideStatus::Completed;
+            driver->SetAvailable(true);
+            cout << "Ride completed\n";
+            cout << "Total price: $" << pricing.Calculate(distance) << endl;
+        }
+    }
+
+    void ShowStatus() const {
+        cout << "Ride status: ";
+        switch (status) {
+        case RideStatus::Requested: cout << "Requested"; break;
+        case RideStatus::Accepted: cout << "Accepted"; break;
+        case RideStatus::InProgress: cout << "In Progress"; break;
+        case RideStatus::Completed: cout << "Completed"; break;
+        }
+        cout << endl;
+    }
+};
